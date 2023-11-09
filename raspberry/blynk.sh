fe25854bb7c2e53bc7600d564dafc75cfd346bf2
samdannyam <61629752+samdannyam@users.noreply.github.com>
2023-11-10 04:50:42 +0800

    blynk

---
+++ raspberry/blynk.sh
@@ -0,0 +1,3 @@
+sudo pip3 install blynk-library-python
+git clone https://github.com/blynkkk/blynk-library.git
+cd blynk-library/linux
--- raspberry/wiringpi.sh
+++ raspberry/wiringpi.sh
@@ -1,3 +1,4 @@
+sudo pip3 install wiringpi
 cd /tmp
 wget https://project-downloads.drogon.net/wiringpi-latest.deb
 sudo dpkg -i wiringpi-latest.deb
