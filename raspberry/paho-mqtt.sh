64c0005cb84e679e1b488429d769cb7321799075
samdannyam <61629752+samdannyam@users.noreply.github.com>
2023-11-10 19:34:02 +0800

    paho-mqtt

---
+++ raspberry/paho-mqtt.sh
@@ -0,0 +1,11 @@
+cd
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential python-dev
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+sudo python setup.py install
+sudo python3 setup.py install
+
+sudo pip intall paho-mqtt
+sudo pip3 install paho-mqtt
